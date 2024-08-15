744a298c2c4fbe3de8b7afe36fa2fc62eb1000ef
Joseph Code <pepet_21@hotmail.com>
2024-08-15 18:15:52 +0200

    Subiendo calculadora js

---
+++ script.js
@@ -0,0 +1,16 @@
+//  Esto agrega a la pantalla el valor del botón de la calculadora que toquemos
+
+function agregar(valor){
+    document.getElementById('pantalla').value += valor
+}
+
+function borrar(){
+    document.getElementById('pantalla').value = ''
+}
+
+function calcular(){
+    const valorPantalla = document.getElementById('pantalla').value
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
+}
+
